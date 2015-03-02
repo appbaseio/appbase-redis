3905a73dacd2b7794292a5f30c6f9dce208adaa4
Siddharth Kothari <sids.aquarius@gmail.com>
2015-03-02 05:35:18 -0600

    appbase data model on redis
    with basic mocha tests

---
+++ appbase_redis.js
@@ -0,0 +1,182 @@
+var redis = require("redis"),
+    client = redis.createClient();
+
+var functions = {}
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+/* Collection methods - create, delete, get */
+
+functions.createCollection = function createCollection(name, done) {
+  var timestamp = Date.now()
+  client.zadd("collections", timestamp, name, function(err, res) {
+    if (err) {
+      done(err)
+    } else {
+      done(null, {"_collection": name, "_createdAt": timestamp})
+    }
+  })
+}
+
+functions.deleteCollection = function deleteCollection(name, done) {
+  client.zrem("collections", name, function(err, res) {
+    if (err) {
+      done(err)
+    } else {
+      done(null, {"_collection": name, "_deleted": true})
+    }
+  })
+}
+
+functions.getCollections = function getCollections(done) {
+  client.zrangebyscore("collections", "-inf", "+inf", function(err, res) {
+    if (err) {
+      done(err)
+    } else {
+      done(null, {"_time": functions._serverTime(), "_collections": res})
+    }
+  })
+}
+
+/* Document methods - create, update, delete, get */
+
+functions.createDocument = function createDocument(collection, name, body, done) {
+  updateDocument(collection, name, body, done)
+}
+
+functions.updateDocument = function updateDocument(collection, name, body, done) {
+  // 3-step process
+  // Step 1: parse the body to separate properties and references.
+  // Step 2: start transaction. apply lock, and update multiple references.
+  // Step 3: Go recursive.
+
+  // step 1
+  functions._parseDocumentBody(body, function(err, res) {
+    if (err) return done(err);
+    // Step 2: Start transaction.
+    c_name = "c`"+collection
+    d_id = "d`"+collection+"`"+name
+    r_id = "r`"+collection+"`"+name
+    var multi = client.multi()
+    // Add all properties to the document d_id
+    for (var key in res.properties) {
+      // TBD: only stringify if an object.
+      multi.hset(d_id, key, JSON.stringify(res.properties[key]), redis.print)
+    }
+    // Always set _id and _collection as the document and collection id and names. Overwrite to ensure no updates.
+    multi.hset(d_id, "_id", name)
+    multi.hset(d_id, "_collection", collection)
+    // Add all the references to the document reference r_id
+    for (var key in res.references) {
+      // There are two cases here.
+      // case-1: res.references[key] points to an actual document. follows...
+      if (typeof res.references[key] !== "string" && res.references[key] !== null) {
+        r_c_name = res.references[key]._collection
+        r_d_id = res.references[key]._id
+        multi.hset(r_id, key, "d`"+r_c_name+"`"+r_d_id)
+        // Now, recursively build up each reference first.
+        functions.updateDocument(r_c_name, r_d_id, res.references[key], function(err, res) {
+          if (err) return done(err);
+        })
+      }
+      // case-2: res.references[key] points to a path. follows...
+      else {
+        // resolve path
+        edgePath = functions._pathResolution(res.references[key])
+        console.log(edgePath)
+        functions.traverse(edgePath[0], edgePath[1], edgePath.slice(2), function(err, c_final, d_final) {
+          if (err) return done(err);
+          multi.hset(r_id, key, "d`"+c_final+"`"+d_final)
+        })
+      }
+
+    }
+    // Add the document in the collection now
+    multi.zadd(c_name, Date.now(), d_id, redis.print)
+    // Execute now
+    multi.exec(function(err, replies) {
+      if (err) return done(err);
+      done(null, replies);
+    })
+  })
+}
+
+functions.getDocument = function getDocument(collection, name, done) {
+  client.hgetall("d`"+collection+"`"+name, function(err, res) {
+    if (err) return done(err);
+    done(null, res)
+  })
+}
+
+functions.deleteDocument = function deleteDocument(collection, name, done) {
+  // what to do here.
+}
+
+functions.traverse = function traverse(collection, name, edgePath, done) {
+  // edge-case when there is no edgePath
+  if (edgePath.length === 0) return done(null, collection, name);
+  client.hget("r`"+collection+"`"+name, edgePath[0], function(err, res) {
+    if (err) return done(err);
+    if (res === null) return done(new Error("Path does not exist"))
+    collection = res.split("`")[1]
+    name = res.split("`")[2]
+    if (edgePath.length == 1) {
+      done(null, collection, name)
+    }
+    else {
+      return functions.traverse(collection, name, edgePath.slice(1), done)
+    }
+  })
+}
+
+// Internal functions
+
+functions._getCollection = function _getCollection(name, done) {
+  client.zscore("collections", name, function(err, memberTimestamp) {
+    if (!memberTimestamp) {
+      done(new Error("Collection name does not exist"), null)
+    } else {
+      done(null, {"_collection": name, "_createdAt": memberTimestamp})
+    }
+  })
+}
+
+functions._serverTime = function _serverTime() {
+  var timeInstance = new Date
+  return {
+    "_timestamp": timeInstance.getTime(),
+    "_timezoneOffset": timeInstance.getTimezoneOffset(),
+    "_ISOString": timeInstance.toISOString()
+  }
+}
+
+functions._parseDocumentBody = function _parseDocumentBody(body, done) {
+  if (body === null || typeof body !== 'object')
+    return done(new Error('Format error in processing request body, not a valid Object'))
+  var res = {'properties': {}, 'references': {}}
+  for (var key in body) {
+    if (body.hasOwnProperty(key)) {
+      // check if reference
+      if (key[0] === '/') {
+        rKey = key.substr(1)
+        if (body[key] !== null) // only update when reference value is not null
+          res['references'][rKey] = body[key]
+      } else {
+        if (body[key] !== null) // only update when key value is not null
+          res['properties'][key] = body[key]
+      }
+    }
+  }
+  done(null, res)
+}
+
+functions._pathResolution = function _pathResolution(path) {
+  // converts something like /a/b//c to ["a", "b", "c"]
+  var edgePath = path.split("/")
+  edgePath = edgePath.filter(function(element) {return element !== ""})
+  return edgePath
+}
+
+module.exports = functions
---
+++ appbase_tests.js
@@ -0,0 +1,147 @@
+var expect = require("chai").expect,
+    ar = require("./appbase_redis.js")
+
+describe("Internal tests", function() {
+  describe("server timestamp", function() {
+    it("should return the time datatypes correctly", function() {
+      var serverTime = ar._serverTime()
+      expect(serverTime).to.have.property('_timestamp').to.be.a('number')
+      expect(serverTime).to.have.property('_timezoneOffset').to.be.a('number')
+      expect(serverTime).to.have.property('_ISOString').to.be.a('string')
+    })
+  })
+
+  describe("Document parser", function() {
+    it("shoud parse properties and references correctly", function(done) {
+      // nothing here so far.
+      var exampleRequestBody = {
+        'hello' : 'js',
+        'everything': true,
+        '/foo' : {
+            '_id': 'anythingworks',
+            '_collection': 'testcollection4238',
+            'this': 'is awesome',
+            'life': 42
+        }
+      }
+      ar.updateDocument('foobar12', 'dabang', exampleRequestBody, function(err, res) {console.log(err, res)})
+      ar._parseDocumentBody(exampleRequestBody, function(err, res) {
+        if (err) throw err;
+        expect(res).to.have.property('properties').to.have.property('hello').to.equal('js')
+        expect(res).to.have.property('properties').to.have.property('everything').to.be.ok
+        expect(res).to.have.property('references').to.have.property('foo').to.have.property('_id').to.equal('anythingworks')
+        expect(res).to.have.property('references').to.have.property('foo').to.have.property('_collection').to.equal('testcollection4238')
+        expect(res).to.have.property('references').to.have.property('foo').to.have.property('this').to.equal('is awesome')
+        expect(res).to.have.property('references').to.have.property('foo').to.have.property('life').to.equal(42)
+        done()
+      })
+    })
+
+    it("should throw an Error when giving incorrect arguments", function() {
+      expect(ar._parseDocumentBody).to.throw(Error)
+    })
+
+    it("should throw an Error when not passing a JavaScript Object type", function(done) {
+      ar._parseDocumentBody(null, function(err, res) {
+        if (err) console.log(err);
+        done();
+      })
+    })
+  })
+
+  describe("Path traverser", function() {
+    it("should traverse the paths correctly and return collection/document", function(done) {
+      ar.traverse("foobar12", "dabang", ["foo"], function(err, collection, name) {
+        if (err) throw err;
+        expect(collection).to.equal('testcollection4238')
+        expect(name).to.equal('anythingworks')
+        done()
+      })
+    })
+    it("should not traverse the paths correctly and return collection/document", function(done) {
+      ar.traverse("foobar12", "dabang", ["foo", "bar"], function(err, collection, name) {
+        if (err) console.log(err);
+        done();
+      })
+    })
+  })
+})
+
+describe("Collection tests", function() {
+  var name = "testcollection1234"
+
+  describe("create collection", function() {
+    it("should create a collection with the current server timestamp", function(done) {
+      ar.createCollection(name, function(err, res) {
+        if (err) throw err;
+        expect(res).to.have.property('_collection').to.equal(name)
+        expect(res).to.have.property('_createdAt').to.be.a('number')
+        done();
+      })
+    })
+  })
+
+  describe("get all collections", function() {
+    it("should return all the collections", function(done) {
+      ar.getCollections(function(err, res) {
+        if (err) throw err;
+        expect(res).to.have.property('_collections').to.be.a('Array')
+        expect(res).to.have.property('_collections').to.have.length.at.least(1)
+        done();
+      })
+    })
+  })
+
+  describe("delete collection", function() {
+    it("should delete the collection with the given key", function(done) {
+      ar.deleteCollection(name, function(err, res) {
+        if (err) throw err;
+        expect(res).to.have.property('_collection').to.equal(name)
+        expect(res).to.have.property('_deleted').to.be.ok
+        done()
+      })
+    })
+  })
+})
+
+describe("Document tests", function() {
+
+  var exampleRequestBody = {
+    'hello' : 'js',
+    'everything': true,
+    '/foo' : {
+        '_id': 'anythingworks',
+        '_collection': 'testcollection4238',
+        'this': 'is awesome',
+        'life': 42
+    }
+  }
+
+  describe("Create / Update a document", function() {
+    it("should create document and reference. reference depth=1, new document=true", function(done) {
+      ar.updateDocument('foobar12', 'dabang', exampleRequestBody, function(err, res) {
+        if (err) throw err;
+        console.log("no error thrown");
+        done();
+      })
+    })
+    it("should create document and reference. reference depth=1, new document=false", function(done) {
+      exampleRequestBody['/foo'] = 'foobar12/dabang'
+      ar.updateDocument('foobar12', 'dabang', exampleRequestBody, function(err, res) {
+        if (err) throw err;
+        console.log("no error thrown");
+        done();
+      })
+    })
+  })
+
+  describe("Get a document", function() {
+    it("should return an existing document", function(done) {
+      ar.getDocument('foobar12', 'dabang', function(err, res) {
+        if (err) throw err;
+        expect(res).to.have.property('hello').to.equal('"js"')
+        done();
+      })
+    })
+  })
+})
